dd9faaae132a29f36b33ccf7b2d3b162b5da443d
wyatt <wppurking@gmail.com>
2015-04-07 14:33:19 +0800

    * add ember root app; * add ember route

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
+// 树的根节点
 var App = Ember.Application.create({
   LOG_TRANSITIONS: true
+});
+
+// 树的枝干, 用于寻找到叶子节点(template)
+App.Router.map(function() {
+  // http://example.com#/about
+  // File Path: none
+  // Ember Path: /about
+  // Router: about
+  //this.route('about');
+
+  // http://example.com#/aboutus
+  // File Path: none
+  // Ember Path: /aboutus
+  // Router: see below
+  this.route('about', {path: '/aboutus'})
+
+  // have a index template default route
 });
